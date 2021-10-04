115a3cb444f92f64e99dd930e0917234e51872b4
Talita Bernardino Neves Silva <talita.silva@db1.com.br>
2021-10-04 10:18:50 -0300

    primeiro commit

---
+++ ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,24 @@
+﻿using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionários;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(847, 489754);
+
+            FuncionarioAutenticavel carlos = null;
+            carlos.Autenticar("asiwqos");
+            Console.WriteLine(conta.Numero);
+
+            Console.ReadLine();
+        }
+    }
+}
